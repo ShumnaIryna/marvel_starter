a9820cc12c64d2002235a8aa5e38941c1b04cc97
ShumnaIryna <87974828+ShumnaIryna@users.noreply.github.com>
2023-02-10 01:49:01 +0200

    added two components MainPages and ComicsPage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import AppBanner from "../appBanner/AppBanner";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import ErrorBoundary from "../errorBoundary/errorBoundary";
-
-import decoration from '../../resources/img/vision.png';
-
 
 const App = () => {
-
-    const [selectedChar, stateChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        stateChar(id);
-    }
 
     return (
         <Router>
@@ -27,22 +12,10 @@ const App = () => {
                 <main>
                     <Switch>
                         <Route exact path="/">
-                            <ErrorBoundary>
-                                <RandomChar/>
-                            </ErrorBoundary>
-                            <div className="char__content">
-                                <ErrorBoundary>
-                                    <CharList onCharSelected={onCharSelected}/>
-                                </ErrorBoundary>
-                                <ErrorBoundary>
-                                    <CharInfo charId={selectedChar}/>
-                                </ErrorBoundary>   
-                            </div>
-                                <img className="bg-decoration" src={decoration} alt="vision"/>
+                            <MainPage/>
                         </Route>
                         <Route exact path="/comics">
-                            <AppBanner/>
-                            <ComicsList/>
+                            <ComicsPage/>
                         </Route>
                     </Switch>
                     </main>
